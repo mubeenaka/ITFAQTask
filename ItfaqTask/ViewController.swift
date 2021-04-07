25253a11dc8ac6564cf1892d26c0a08fb60b2bfb
mubeenaka <mubeena.ka@gmail.com>
2021-04-07 15:40:24 +0400

    Initial Commit

---
+++ ItfaqTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItfaqTask
+//
+//  Created by Mubeena Thahir on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
